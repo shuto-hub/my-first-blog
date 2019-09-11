8356de688d604d3a7230bc1c2016363ef06ea2a7
Your Name <you@example.com>
2019-09-11 23:33:12 +0900

    login view test

--- blog/views.py
+++ blog/views.py
@@ -7,6 +7,7 @@ from django.contrib.auth import authenticate,logout, login as django_login
 from django.contrib.auth.models import User
 from . import forms
 
+@login_required
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     user = request.user
