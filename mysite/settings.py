e137d42e50d78502d3cb6eb380abd9c3f99bed8c
Your Name <you@example.com>
2019-09-11 15:16:27 +0900

    revise

--- mysite/settings.py
+++ mysite/settings.py
@@ -24,6 +24,21 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+            'level': os.getenv('DJANGO_LOG_LEVEL', 'DEBUG'),
+        },
+    },
+}
 
 ALLOWED_HOSTS = ['*']
 ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
