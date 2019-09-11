27a56cbf5d780d8febef2e723f8de445844c7e34
Your Name <you@example.com>
2019-09-11 14:43:09 +0900

    revise

--- mysite/settings.py
+++ mysite/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
 # Application definition
 
 INSTALLED_APPS = [
+    'whitenoise.runserver_nostatic',
     'accounts.apps.AccountsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
