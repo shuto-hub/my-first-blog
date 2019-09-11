9d25bf732bfe0252d84a94ed66cc0d033caad09d
Your Name <you@example.com>
2019-09-11 22:48:40 +0900

    revise

--- mysite/settings.py
+++ mysite/settings.py
@@ -150,7 +150,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 try:
     from .local_settings import *
 except ImportError:
