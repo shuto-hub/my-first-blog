5596d89196b0e6b2e89b9e18e9492486ab74c6d3
Your Name <you@example.com>
2019-09-12 12:58:23 +0900

    Complete!

--- blog/views.py
+++ blog/views.py
@@ -6,26 +6,34 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth import authenticate,logout, login as django_login
 from django.contrib.auth.models import User
 from . import forms
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.template.response import TemplateResponse
 
 @login_required
 def post_list(request):
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
     user = request.user
-    return render(request, 'blog/post_list.html', {'posts': posts})
+    paginator = Paginator(posts, 5)
+    page = request.GET.get('page', 1)
+    try:
+        posts = paginator.page(page)
+    except (EmptyPage, PageNotAnInteger):
+        posts = paginator.page(1)
+    return TemplateResponse(request, 'blog/post_list.html', {'posts': posts})
 
 @login_required
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     user = request.user
     comments = Comment.objects.filter(post=post)
-    if request.method == "POST": #入力フォームはPOSTなので
+    if request.method == "POST": 
         form = forms.CommentForm(request.POST)
-        if form.is_valid(): #もし、formの内容が正しい時は
-            comment = form.save(commit=False) #formの内容はまだセーブしません！
+        if form.is_valid():
+            comment = form.save(commit=False)
             comment.author = request.user
             comment.post = post
             comment.published_date = timezone.now()
-            comment.save() #ユーザーを追加したのちにセーブ
+            comment.save()
     else:
         form = forms.CommentForm()
     return render(request, 'blog/post_detail.html', {'post': post,'form': form,'comments': comments})
